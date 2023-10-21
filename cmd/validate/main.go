7eb96c8904e021128e83016159be0f110f1fac2f
NxPKG <116948796+NxPKG@users.noreply.github.com>
2023-10-21 01:56:32 +0000

    Update main.go

--- cmd/validate/main.go
+++ cmd/validate/main.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/invopop/yaml"
 
-	"github.com/getkin/kin-openapi/openapi2"
-	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/gsoc2/kin-openapi/openapi2"
+	"github.com/gsoc2/kin-openapi/openapi3"
 )
 
 var (
